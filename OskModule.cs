356f004e50ed0b9195789d96872a8a10d74f6c4c
Toby McLaughlin <toby@jarpy.net>
2015-05-29 20:53:59 +1000

    Cap telemetry rate at 10 updates/second.

--- OskModule.cs
+++ OskModule.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Net;
 using System.Net.Sockets;
 
@@ -7,30 +8,35 @@ namespace OSK
     public class OskModule : PartModule
     {
         private UdpClient client = new UdpClient();
-        private IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9599);
+        private IPEndPoint server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9599);
+        private Int64 last_transmit_time;
+        private const int ticks_per_millisecond = 10000;
 
         public override void OnStart(StartState state)
         {
-            client.Connect(ep);
+            last_transmit_time = 0;
+            client.Connect(server);
         }
 
         public override void OnUpdate()
         {
             base.OnUpdate();
-            transmit("altitude", vessel.altitude.ToString());
+
+            // Cap the transmission rate at 10 updates per second.
+            Int64 now = DateTime.Now.Ticks;
+            if (now > (last_transmit_time + (ticks_per_millisecond * 100)))
+            {
+                transmit("altitude", vessel.altitude.ToString());
+                transmit("vertical_speed", vessel.verticalSpeed.ToString());
+                last_transmit_time = DateTime.Now.Ticks;
+            }
         }
 
         private void transmit(string key, string value)
         {
-            send_string(key + "=" + value);
-        }
-
-        private void send_string(string s)
-        {
-            s = s + "\n";
-            byte[] payload = System.Text.Encoding.ASCII.GetBytes(s);
-            client.Send(payload, payload.Length);
-
+            string message = key + "=" + value + "\n";
+            byte[] network_payload = System.Text.Encoding.ASCII.GetBytes(message);
+            client.Send(network_payload, network_payload.Length);
         }
     }
 }
