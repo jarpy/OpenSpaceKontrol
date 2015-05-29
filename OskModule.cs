bd0b2b81e5b2bc8c2bfe7ce110d9a8471cbae462
Toby McLaughlin <toby@jarpy.net>
2015-05-29 19:04:36 +1000

    Networking proof of concept.
    Added a part that continuously transmits vessel altitude over UDP.

---
+++ OskModule.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+namespace OSK
+{
+    public class OskModule : PartModule
+    {
+        private UdpClient client = new UdpClient();
+        private IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9599);
+
+        public override void OnStart(StartState state)
+        {
+            client.Connect(ep);
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            transmit("altitude", vessel.altitude.ToString());
+        }
+
+        private void transmit(string key, string value)
+        {
+            send_string(key + "=" + value);
+        }
+
+        private void send_string(string s)
+        {
+            s = s + "\n";
+            byte[] payload = System.Text.Encoding.ASCII.GetBytes(s);
+            client.Send(payload, payload.Length);
+
+        }
+    }
+}
